bf376514d0361f314930f092db6ab0da522bf36d
Stepan Zastupov <stepan.zastupov@gmail.com>
2017-04-09 03:34:30 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,21 @@ from setuptools import setup
 
 setup(
     name='setmeup',
-    description='Runtime settings for minimalists',
     version='0.1',
+    description='Runtime settings for minimalists',
+    url='https://github.com/szastupov/setmeup',
+    license='MIT',
+    author='Stepan Zastupov',
+    author_email='stepan.zastupov@gmail.com',
+
     py_modules=['setmeup'],
-    license='MIT'
+
+    keywords='environ settings env config',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries'
+    ]
 )
